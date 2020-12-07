39b5c984d9d0e720e5cc67ae2154eb0e302db55e
jashjchoi <jashjchoi@gmail.com>
2020-12-07 22:32:26 +0000

    Fix the format and add -f flag

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 # Using Puppet, create a manifest that kills a process killmenow
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  provider => shell,
+exec { 'pkill killmenow':
+  command => '/usr/bin/pkill -f /killmenow'
 }
