e65365a51e641c14532081d9a1b23ebec6f94c6b
jashjchoi <jashjchoi@gmail.com>
2021-02-01 23:35:37 +0000

    Debugging

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     USER_ID = argv[1]
     FILE_NAME = USER_ID + '.csv'
     url_emp = 'https://jsonplaceholder.typicode.com/users/{}'\
-        .format(int(argv[1]))
+        .format(int(USER_ID))
     EMPLOYEE_NAME = requests.get(url_emp).json()
     url_todos = ("https://jsonplaceholder.typicode.com/todos")
     req_todos = requests.get(url_todos).json()
-    with open(FILE_NAME, mode='w', newline='') as csv_file:
+    with open(FILE_NAME, mode='w+', newline='') as csv_file:
         c_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
                               quoting=csv.QUOTE_ALL)
         for todo in req_todos:
