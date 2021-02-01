2cf0c69374c02fbef39ea78c86296eb8d21e4ec2
jashjchoi <jashjchoi@gmail.com>
2021-02-01 23:02:48 +0000

    Change the file name to USER_ID.csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,10 +9,10 @@ import csv
 
 if __name__ == "__main__":
     """display the list of username and tasks in csv file"""
-    id_emp = argv[1]
-    FILE_NAME = id_emp + '.csv'
+    USER_ID = argv[1]
+    FILE_NAME = USER_ID + '.csv'
     url_emp = 'https://jsonplaceholder.typicode.com/users/{}'\
-        .format(int(id_emp))
+        .format(int(argv[1]))
     EMPLOYEE_NAME = requests.get(url_emp).json()
     url_todos = ("https://jsonplaceholder.typicode.com/todos")
     req_todos = requests.get(url_todos).json()
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         c_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
                               quoting=csv.QUOTE_ALL)
         for todo in req_todos:
-            if todo.get('userId') == int(id_emp):
+            if todo.get('userId') == int(USER_ID):
                 c_writer.writerow([todo.get("userId"),
                                     EMPLOYEE_NAME.get("username"),
                                     todo.get("completed"),
