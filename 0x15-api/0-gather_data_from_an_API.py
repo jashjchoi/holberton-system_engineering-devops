075c5bef16821441392f3cc5e4af2f6e0e3c4fe2
jashjchoi <jashjchoi@gmail.com>
2021-02-01 22:39:04 +0000

    Change the variable name/format to debug the error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,20 +3,29 @@
     Uses the fake API for a given employee ID
     returns information about todo list progress.
 """
-from sys import argv
 import requests
+from sys import argv
+
 
 if __name__ == "__main__":
+    """display the title of completed tasks"""
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_LIST = []
     id_emp = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_emp)
-    url_todos = url_employ + "/todos"
-    req_employ = requests.get(url_employ).json()
+    url_emp = 'https://jsonplaceholder.typicode.com/users/{}'\
+        .format(int(id_emp))
+    EMPLOYEE_NAME = requests.get(url_emp).json()
+    print("Employee {} is done with tasks"
+          .format(EMPLOYEE_NAME.get('name')), end="")
+    url_todos = ("https://jsonplaceholder.typicode.com/todos")
     req_todos = requests.get(url_todos).json()
-    name = req_employ.get("name")
-    num_task = req_todos
-    done_task = [task for task in req_todos if task.get("completed")]
-    res = "Employee {} is done with tasks({}/{}):".format(
-                name, len(done_task), len(num_task))
-    for task in done_task:
-        res += "\n\t " + task.get("title")
-    print(res)
+    for todo in req_todos:
+        if todo.get('userId') == int(id_emp):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if todo.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_LIST.append(todo.get("title"))
+    print('({}/{}):'.format(NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for TASK_TITLE in TASK_LIST:
+        print('\t {}'.format(TASK_TITLE))
