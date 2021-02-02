8f0e457f082e04248b1f218e17d92d48e77ac7da
jashjchoi <jashjchoi@gmail.com>
2021-02-02 04:47:50 +0000

    Modify task 3 to add the username in the output dictionary

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,20 +7,18 @@ from sys import argv
 import json
 
 if __name__ == '__main__':
-    url_user = 'https://jsonplaceholder.typicode.com/users'
-    users = get(url_user).json()
+    req_user = get('https://jsonplaceholder.typicode.com/users').json()
     result = {}
-    for usr in users:
-        id_emp = usr.get("id")
-        url_todos = url_user + "/{}/todos".format(id_emp)
-        req_todos = get(url_todos).json()
-        username = usr.get("username")
+    for usr in req_user:
+        id_emp = usr["id"]
+        req_todos = get('https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(id_emp)).json()
         ALL_LIST = []
         for todo in req_todos:
             dict_task = {}
-            dict_task["username"] = str(username)
+            dict_task["username"] = usr.get("username")
             dict_task["completed"] = todo.get("completed")
-            dict_task["task"] = str(todo.get("title"))
+            dict_task["task"] = todo.get("title")
             ALL_LIST.append(dict_task)
         result[id_emp] = ALL_LIST
     with open("todo_all_employees.json", mode='w+', newline='') as json_file:
