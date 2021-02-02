7c14028c05755df024bdd42808f65190d42d3a90
jashjchoi <jashjchoi@gmail.com>
2021-02-02 00:38:36 +0000

    Add task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Uses the fake API for a given employee ID
+    export data as JSON
+"""
+from requests import get
+from sys import argv
+import json
+
+if __name__ == '__main__':
+    url_user = 'https://jsonplaceholder.typicode.com/users'
+    users = get(url_user).json()
+    result = {}
+    for usr in users:
+        id_emp = usr.get("id")
+        url_todos = url_user + "/{}/todos".format(id_emp)
+        req_todos = get(url_todos).json()
+        username = usr.get("username")
+        ALL_LIST = []
+        for todo in req_todos:
+            dict_task = {}
+            dict_task["username"] = str(username)
+            dict_task["completed"] = todo.get("completed")
+            dict_task["task"] = str(todo.get("title"))
+            ALL_LIST.append(dict_task)
+        result[id_emp] = ALL_LIST
+    with open("todo_all_employees.json", mode='w+', newline='') as json_file:
+        json_file.write(json.dumps(result))
